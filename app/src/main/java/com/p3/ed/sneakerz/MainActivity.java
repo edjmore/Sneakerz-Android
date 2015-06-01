6488339d76b7ebf4829ddeba1fcc7588783ba3a9
Ed Moore <edmoore7@gmail.com>
2015-06-01 11:55:54 -0400

    Cleaned up main activity code.

--- app/src/main/java/com/p3/ed/sneakerz/MainActivity.java
+++ app/src/main/java/com/p3/ed/sneakerz/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
@@ -21,34 +20,54 @@ import java.sql.SQLException;
 public class MainActivity extends ActionBarActivity {
     public static final String TAG = "MainActivity";
 
-    private DataSrc mDataSrc;
-
-    private ListView mShoeList;
     private Cursor mShoeCursor;
-    private ShoeCursorAdapter mShoeAdapter;
 
-    private EditText mEditText;
+    private final View.OnClickListener mNewShoeButtonListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            EditText editText = (EditText) findViewById(R.id.main_shoe_name_input);
+            String input = editText.getText().toString();
+            if (!input.isEmpty()) {
+                DataSrc dataSrc = new DataSrc(getApplicationContext());
+                try {
+                    dataSrc.open();
+                    dataSrc.addShoe(input);
+
+                    mShoeCursor = dataSrc.getAllShoes();
+                } catch (SQLException sqle) {
+                    sqle.printStackTrace();
+                } finally {
+                    if (dataSrc.isOpen()) dataSrc.close();
+                }
+            }
+
+            refreshViews();
+        }
+    };
+
+    private final AdapterView.OnItemClickListener mLvClkListener = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            int _id = view.getId();
+
+            Intent intent = new Intent(MainActivity.this, ViewShoeActivity.class);
+            intent.putExtra(ViewShoeActivity.KEY_SHOE_ID, _id);
+            startActivity(intent);
+        }
+    };
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
 
-        // Initialize database
-        mDataSrc = new DataSrc(this);
+        Button newShoeButton = (Button) findViewById(R.id.main_add_shoe_button);
+        newShoeButton.setOnClickListener(mNewShoeButtonListener);
 
-        // Get handles for views
-        mShoeList = (ListView) findViewById(R.id.main_shoe_list);
-        mEditText = (EditText) findViewById(R.id.main_shoe_name_input);
+        ListView shoeList = (ListView) findViewById(R.id.main_shoe_list);
+        shoeList.setOnItemClickListener(mLvClkListener);
 
-        Button button = (Button) findViewById(R.id.main_add_shoe_button);
-        // Add a new row to the database when button is clicked
-        button.setOnClickListener(buttonClickListener);
-
-        // Open edit shoe activity when list item is clicked
-        mShoeList.setOnItemClickListener(lvClickListener);
-
-        // Load data into list view
+        loadShoeCursor();
         refreshViews();
     }
 
@@ -63,66 +82,37 @@ public class MainActivity extends ActionBarActivity {
     public void onResume() {
         super.onResume();
 
+        loadShoeCursor();
         refreshViews();
-    }
-
-    private final View.OnClickListener buttonClickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            String input = mEditText.getText().toString();
-            // Only add a new shoe if input is not empty
-            if (!input.isEmpty()) {
-                try {
-                    mDataSrc.open();
-                    mDataSrc.addShoe(input);
-                } catch (SQLException sqle) {
-                    sqle.printStackTrace();
-                }
-            }
-
-            // Refresh list view
-            refreshViews();
-        }
-    };
-
-    private final AdapterView.OnItemClickListener lvClickListener = new AdapterView.OnItemClickListener() {
-        @Override
-        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            // Get the shoe's ID from view
-            int _id = view.getId();
-
-            // Start edit shoe activity and pass the ID along
-            Intent intent = new Intent(MainActivity.this, ViewShoeActivity.class);
-            intent.putExtra(ViewShoeActivity.KEY_SHOE_ID, _id);
-            startActivity(intent);
-        }
-    };
-
-    private void refreshViews() {
-        // Get all shoe data
-        try {
-            mDataSrc.open();
-            mShoeCursor = mDataSrc.getAllShoes();
-        } catch (SQLException sqle) {
-            sqle.printStackTrace();
-        }
-
-        // Bind to the list view (if not null or empty)
-        if (mShoeCursor != null && mShoeCursor.getCount() > 0) {
-            mShoeCursor.moveToNext();
-            mShoeAdapter = new ShoeCursorAdapter(this, mShoeCursor, 0);
-            mShoeList.setAdapter(mShoeAdapter);
-        }
-
-        // Clear text
-        mEditText.setText("");
     }
 
     @Override
     public void onDestroy() {
-        // Need to make sure nothing is leaked
-        if (mDataSrc != null && mDataSrc.isOpen()) mDataSrc.close();
-
         super.onDestroy();
+    }
+
+    private void loadShoeCursor() {
+        DataSrc dataSrc = new DataSrc(this);
+        try {
+            dataSrc.open();
+            mShoeCursor = dataSrc.getAllShoes();
+        } catch (SQLException sqle) {
+            sqle.printStackTrace();
+        } finally {
+            if (dataSrc.isOpen()) dataSrc.close();
+        }
+    }
+
+    private void refreshViews() {
+        if (mShoeCursor != null && mShoeCursor.getCount() > 0) {
+            mShoeCursor.moveToNext();
+
+            ListView shoeList = (ListView) findViewById(R.id.main_shoe_list);
+            ShoeCursorAdapter shoeCursorAdapter = new ShoeCursorAdapter(this, mShoeCursor, 0);
+            shoeList.setAdapter(shoeCursorAdapter);
+        }
+
+        EditText editText = (EditText) findViewById(R.id.main_shoe_name_input);
+        editText.clearComposingText();
     }
 }
