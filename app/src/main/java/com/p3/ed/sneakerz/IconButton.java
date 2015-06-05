c7bb6cf82c244e4c50d55d51625ed1bfc44bd5ad
Ed Moore <edmoore7@gmail.com>
2015-06-05 13:00:51 -0400

    Major popup window styling on main activity. Fading background.

--- app/src/main/java/com/p3/ed/sneakerz/IconButton.java
+++ app/src/main/java/com/p3/ed/sneakerz/IconButton.java
@@ -10,6 +10,7 @@ import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 
 /**
--- app/src/main/java/com/p3/ed/sneakerz/MainActivity.java
+++ app/src/main/java/com/p3/ed/sneakerz/MainActivity.java
@@ -1,22 +1,26 @@
 package com.p3.ed.sneakerz;
 
+import android.app.ActionBar;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Point;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.ListView;
 import android.widget.PopupWindow;
+import android.widget.Toast;
 
 import java.sql.SQLException;
 
@@ -28,41 +32,40 @@ public class MainActivity extends ActionBarActivity {
 
 
     private Context mContext;
+    private Point mDisplaySize;
     private ShoeCursorAdapter mAdapter;
     private ListView mListView;
-
-    private final View.OnClickListener mNewShoeButtonListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            showPopup();
-        }
-    };
-
-    private final AdapterView.OnItemClickListener mLvClkListener = new AdapterView.OnItemClickListener() {
-        @Override
-        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            int _id = view.getId();
-
-            Intent intent = new Intent(MainActivity.this, ViewShoeActivity.class);
-            intent.putExtra(ViewShoeActivity.KEY_SHOE_ID, _id);
-            startActivity(intent);
-        }
-    };
+    private PopupWindow mPopupWindow;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
-        mContext = this;
+        init();
 
-        // Handle list view first
+        // List of shoes
         mListView = (ListView) findViewById(R.id.main_shoe_list);
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                int _id = view.getId();
+                // Launch a shoe detail activity using the shoe's ID
+                Intent intent = new Intent(MainActivity.this, ViewShoeActivity.class);
+                intent.putExtra(ViewShoeActivity.KEY_SHOE_ID, _id);
+                startActivity(intent);
+            }
+        });
+        // Get data and populate the list
         refresh();
-        mListView.setOnItemClickListener(mLvClkListener);
 
         // Button for adding new shoes to list
-        Button newShoeButton = (Button) findViewById(R.id.main_add_shoe_button);
-        newShoeButton.setOnClickListener(mNewShoeButtonListener);
+        findViewById(R.id.main_add_shoe_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Show a popup window to create a new shoe
+                showPopup();
+            }
+        });
     }
 
     @Override
@@ -77,6 +80,7 @@ public class MainActivity extends ActionBarActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_settings:
+                // Launch settings activity
                 Intent prefs = new Intent(this, PrefActivity.class);
                 startActivity(prefs);
                 return true;
@@ -87,8 +91,18 @@ public class MainActivity extends ActionBarActivity {
     @Override
     public void onResume() {
         super.onResume();
-
+        // Get new data and refresh the shoe list
         refresh();
+    }
+
+    private void init() {
+        mContext = this;
+        // Need to set background to transparent
+        FrameLayout root = (FrameLayout) findViewById(R.id.main_root);
+        root.getForeground().setAlpha(0);
+        // Screen size
+        mDisplaySize = new Point();
+        getWindowManager().getDefaultDisplay().getSize(mDisplaySize);
     }
 
     private void refresh() {
@@ -110,49 +124,61 @@ public class MainActivity extends ActionBarActivity {
         }
     }
 
-    private PopupWindow mPopupWindow;
-
     private void showPopup() {
-        LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
-        View view = inflater.inflate(R.layout.new_shoe_popup, null);
-        Button done = (Button) view.findViewById(R.id.new_shoe_popup_finish);
-        done.setOnClickListener(new View.OnClickListener() {
+        View popupView = getLayoutInflater().inflate(R.layout.new_shoe_popup, null);
+        // Button to finish creating a new shoe
+        Button doneButton = (Button) popupView.findViewById(R.id.new_shoe_popup_finish);
+        doneButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                View parent = (View) v.getParent();
-                EditText editText = (EditText) parent.findViewById(R.id.new_shoe_popup_name);
-                String input = editText.getText().toString();
+            public void onClick(View view) {
+                // Get user input
+                EditText editName = (EditText) mPopupWindow.getContentView()
+                        .findViewById(R.id.new_shoe_popup_name);
+                String input = editName.getText().toString();
+                // Dismiss the popup
                 mPopupWindow.dismiss();
-                if (!input.isEmpty()) {
-                    DataSrc dataSrc = new DataSrc(mContext);
-                    try {
-                        dataSrc.open();
-                        // Add new shoe
-                        dataSrc.addShoe(input);
-                        // Refresh views while data source is open
-                        Cursor cursor = dataSrc.getAllShoes();
-                        // Adapter will not be null
-                        mAdapter.swapCursor(cursor);
-                    } catch (SQLException sqle) {
-                        sqle.printStackTrace();
-                    } finally {
-                        if (dataSrc.isOpen()) dataSrc.close();
-                    }
+                // Restore backround
+                FrameLayout root = (FrameLayout) findViewById(R.id.main_root);
+                root.getForeground().setAlpha(0); // 0 is transparent
+
+                boolean success = createNewShoe(input);
+                // Let user know if new shoe was added
+                if (success) {
+                    Toast.makeText(mContext, "\'" + input + "\' added!", Toast.LENGTH_SHORT);
+                    refresh();
+                } else {
+                    Toast.makeText(mContext, "Shoe not added.", Toast.LENGTH_SHORT);
                 }
             }
         });
-        Point size = new Point();
-        getWindowManager().getDefaultDisplay().getSize(size);
-        int width = (int) (size.x * 0.8);
-        int height = (int) (size.y * 0.4);
-        boolean focusable = true;
-        mPopupWindow = new PopupWindow(view, width, height, focusable);
-        mPopupWindow.setOnDismissListener(new PopupWindow.OnDismissListener() {
-            @Override
-            public void onDismiss() {
-                // TODO: Do something?
+
+        // Use screen dimensions to calculate popup window size
+        mPopupWindow = new PopupWindow(popupView, (int) (mDisplaySize.x * 0.85),
+                ViewGroup.LayoutParams.WRAP_CONTENT, true);
+        // Show ar center of shoe list view
+        mPopupWindow.showAtLocation(findViewById(R.id.main_shoe_list),
+                Gravity.TOP | Gravity.CENTER_HORIZONTAL, 0, (int) (mDisplaySize.y * 0.25));
+        // Dim the background
+        FrameLayout root = (FrameLayout) findViewById(R.id.main_root);
+        root.getForeground().setAlpha(208); // 255 is invisible
+    }
+
+    private boolean createNewShoe(String input) {
+        if (!input.isEmpty()) {
+            DataSrc dataSrc = new DataSrc(mContext);
+            try {
+                dataSrc.open();
+                // Write new shoe to database
+                dataSrc.addShoe(input);
+                // Shoe successfully added
+                return true;
+            } catch (SQLException sqle) {
+                sqle.printStackTrace();
+            } finally {
+                if (dataSrc.isOpen()) dataSrc.close();
             }
-        });
-        mPopupWindow.showAtLocation(findViewById(R.id.main_shoe_list), Gravity.CENTER, 0, 0);
+        }
+        // There was an error and no new shoe was created
+        return false;
     }
 }
